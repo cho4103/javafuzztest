0777ae0c257cab0ecbabb058b8a9b95fec607d9d
hoyeol_choi <hoyeol_choi@tmax.co.kr>
2021-01-12 16:38:39 +0900

    test commit

--- example/src/test/java/hy_java_fuzz_test/example/AppTest.java
+++ example/src/test/java/hy_java_fuzz_test/example/AppTest.java
@@ -1,12 +1,18 @@
 package hy_java_fuzz_test.example;
 
+import dev.fuzzit.javafuzz.core.AbstractFuzzTarget;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-	 public void fuzz(byte[] data)
+	public class FuzzParseComplex extends AbstractFuzzTarget
+	{
+	    public void fuzz(byte[] data)
 	    {
 	        App.parseComplex(data);
 	    }
+	}
+
 }
