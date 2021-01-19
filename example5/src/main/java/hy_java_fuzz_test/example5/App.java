19523a6bea42ad8990e8e3425271332dd4adddc9
hoyeol_choi <hoyeol_choi@tmax.co.kr>
2021-01-19 12:17:05 +0900

    test commit

--- example5/src/main/java/hy_java_fuzz_test/example5/App.java
+++ example5/src/main/java/hy_java_fuzz_test/example5/App.java
@@ -7,7 +7,7 @@ package hy_java_fuzz_test.example5;
 public class App 
 {
 	public static String parseComplex (byte[] data) {
-        if (data.length >= 3) {
+        if (data.length < 3) {
             if (Byte.compare(data[0],(byte)0x46) == 0 && Byte.compare(data[1], (byte)0x55) == 0 && Byte.compare(data[2], (byte)0x5a) == 0 &&
                     data[3] == (byte) 0x5a) {
                 return "this will throw out of bound exception";
