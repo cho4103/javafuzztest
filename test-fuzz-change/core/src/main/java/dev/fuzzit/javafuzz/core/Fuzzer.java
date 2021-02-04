79c976409da2ab200be873bd648cc3b1862552bd
hoyeol_choi <hoyeol_choi@tmax.co.kr>
2021-02-04 15:01:45 +0900

    tc

--- test-fuzz-change/core/src/main/java/dev/fuzzit/javafuzz/core/Fuzzer.java
+++ test-fuzz-change/core/src/main/java/dev/fuzzit/javafuzz/core/Fuzzer.java
@@ -34,8 +34,9 @@ public class Fuzzer {
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         md.update(buf);
         byte[] digest = md.digest();
-        String hex = String.format("%064x", new BigInteger(1, digest));
-        String filepath = "crash-" + hex;
+//      String hex = String.format("%064x", new BigInteger(1, digest));
+//      String filepath = "crash-" + hex;
+        String filepath = "crash";
         try (FileOutputStream fos = new FileOutputStream(filepath)) {
             fos.write(buf);
             System.out.printf("crash was written to %s\n", filepath);
